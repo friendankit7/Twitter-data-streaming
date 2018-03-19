f01c232a7a0b2f7d29bfbc89ed0e38229e1ecc28
Ankit Goyal <ankit.goyal@nagarro.com>
2018-03-19 12:40:39 +0530

    Main class added

---
+++ twitter-streaming/src/main/java/com/twitter/streaming/TwitterStreamingApplication.java
@@ -0,0 +1,15 @@
+package com.twitter.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ankitgoyal
+ */
+@SpringBootApplication
+public class TwitterStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterStreamingApplication.class, args);
+	}
+}
